899e6b9588f957480938458315e0db7b04bf1dc3
ayaka <ayakaob@gmail.com>
2022-01-23 11:53:19 -0800

    create GreetingScreen and CustomButton

--- App.js
+++ App.js
@@ -4,13 +4,19 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 import LoginScreen from "./src/screens/LoginScreen";
+import GreetingScreen from "./src/screens/GreetingScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Greeting">
+        <Stack.Screen
+          name="Greeting"
+          component={GreetingScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen
           name="Login"
           component={LoginScreen}
