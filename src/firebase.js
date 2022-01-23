3ba375be483ebbb696a8a74ca4d767c0fdba390a
ayaka <ayakaob@gmail.com>
2022-01-22 18:28:33 -0800

    set up firebase auth

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { FIREBASE_API_KEY } from "@env";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "capstone-4d645.firebaseapp.com",
+  projectId: "capstone-4d645",
+  storageBucket: "capstone-4d645.appspot.com",
+  messagingSenderId: "588506380822",
+  appId: "1:588506380822:web:5b89ced6193e8f63b55337",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
