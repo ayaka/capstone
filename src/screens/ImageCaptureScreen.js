6f4b6951ac93a708c66355401a1e180dae02ea7d
ayaka <ayakaob@gmail.com>
2022-02-08 15:13:59 -0800

    fix crashing expo app problem by resizing image before upload

--- src/screens/ImageCaptureScreen.js
+++ src/screens/ImageCaptureScreen.js
@@ -1,83 +1,72 @@
-import { Button, Image, StyleSheet, View } from "react-native";
-import React, { useState } from "react";
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
-import { useRoute } from "@react-navigation/native";
+import { manipulateAsync, SaveFormat } from "expo-image-manipulator";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
-import { ref, uploadBytes } from "firebase/storage";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { storage } from "../firebase";
 
 const ImageCaptureScreen = () => {
+  const navigation = useNavigation();
   const route = useRoute();
   // const [image, setImage] = useState(null);
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
-      aspect: [4, 3],
-      quality: 0,
+      aspect: [1, 1],
+      quality: 1,
     });
 
     if (!result.cancelled) {
-      uploadImage(result.uri);
+      try {
+        const compressedImage = await manipulateAsync(
+          result.uri,
+          [
+            {
+              resize: {
+                height: 300,
+              },
+            },
+          ],
+          {
+            compress: 1,
+          }
+        );
+
+        const url = await uploadImage(compressedImage.uri);
+        console.log(url);
+      } catch (error) {
+        alert(error.message);
+      }
     }
   };
 
   const uploadImage = async (uri) => {
     const imageRef = ref(storage, `${route.params.petId}`);
+
     const response = await fetch(uri);
     const blob = await response.blob();
 
     const uploadTask = await uploadBytes(imageRef, blob);
-
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
-        }
-      },
-      (error) => {
-        // A full list of error codes is available at
-        // https://firebase.google.com/docs/storage/web/handle-errors
-        switch (error.code) {
-          case "storage/unauthorized":
-            // User doesn't have permission to access the object
-            break;
-          case "storage/canceled":
-            // User canceled the upload
-            break;
-
-          // ...
-
-          case "storage/unknown":
-            // Unknown error occurred, inspect error.serverResponse
-            break;
-        }
-      },
-      () => {
-        // Upload completed successfully, now we can get the download URL
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log("File available at", downloadURL);
-        });
-      }
-    );
+    const downloadURL = await getDownloadURL(uploadTask.ref);
+    return downloadURL;
   };
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
       {/* {image && (
-          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
       )} */}
     </View>
   );
